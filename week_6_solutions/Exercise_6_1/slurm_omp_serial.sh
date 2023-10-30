7e5197e60203b63c4755c9ef60704158c18d1835
Chiara Zanatta <czanatta@eiger-ln001.cscs.ch>
2023-10-30 20:49:59 +0100

    Started week 6

---
+++ week_6_solutions/Exercise_6_1/slurm_omp_serial.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --job-name="serial"
+#SBATCH --account="uzh8"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=chiara.zanatta@uzh.ch
+#SBATCH --time=00:15:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --partition=debug
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+srun serial
