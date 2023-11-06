1869079dbee0db23031dd4c1dfcf41c0e5d94ab2
Chiara Zanatta <czanatta@eiger-ln004.cscs.ch>
2023-11-06 23:26:40 +0100

    Exercise 7.1

--- week_7_solutions/Exercise_7_1/ring_non_block.c
+++ week_7_solutions/Exercise_7_1/ring_non_block.c
@@ -7,8 +7,8 @@ int main(int argc, char** argv) {
 
     // Get the number of processes and rank of the process
     int size, my_rank, tag = 100;
-    MPI_Status status;
-    MPI_Request request;
+    MPI_Status status[2];
+    MPI_Request request[2];
     MPI_Comm_size(MPI_COMM_WORLD, &size); // gets the number of MPI processes in the communicator in which the calling MPI process belongs
     // returns a number for size, basically it the function goes into the communicater and gets the number of processes and assigns it to the
     // variable size -> is the number of processes
@@ -21,7 +21,15 @@ int main(int argc, char** argv) {
     int my_sum = my_rank; // initalize with own rank
 
     int send_rank = my_rank;  // Send buffer
-    int recv_rank = 0;        // Receive buffer
+    int recv_rank;    // Receive buffer
+    
+    // initialize the receive rank, from which rank should data be received
+    if (my_rank == 0) {
+	    recv_rank = size;
+    }
+    else {
+	    recv_rank = my_rank - 1;
+    }
 
     // Compute the ranks of left/right neighbours 
     int left_rank, right_rank;
@@ -43,14 +51,23 @@ int main(int argc, char** argv) {
     //     update the send buffer
     //     update the local sum
     for (int i = 0; i < n_proc-1; i++) {
-        MPI_Isend(&send_rank, 1, MPI_INTEGER, right_rank, 100, MPI_COMM_WORLD, &request);
-        MPI_Irecv(&recv_rank, 1, MPI_INTEGER, left_rank, 100, MPI_COMM_WORLD, &request);
-        my_sum += recv_rank;
+	    // send from the current rank to the rank to the right, the value that should be sent
+	    // is the value for the own rank
+	    MPI_Isend(&send_rank, 1, MPI_INTEGER, right_rank, 100, MPI_COMM_WORLD, &request[0]);
+	    // receive from the rank to the left, the value that should be received is the value
+	    // for the rank of the left rank
+	    MPI_Irecv(&recv_rank, 1, MPI_INTEGER, left_rank, 100, MPI_COMM_WORLD, &request[1]);
+	    // wait till all is said and done
+	    MPI_Waitall(2, request, status);
+	    // update the sum
+	    my_sum += recv_rank;
+	    // the value that should be sent is the one that has been received before
+	    send_rank = recv_rank;
     }
 
     printf("I am processor %d out of %d, and the sum is %d\n", my_rank, size, my_sum);
-    MPI_Waitall(&request, &status)
 
     // Finalize the MPI environment.
     MPI_Finalize();
+    return 0;
 }
