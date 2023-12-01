f04848b7275d37510283a03f8e6af3caa366e828
Chiara Zanatta <chiara.zanatta@hispeed.ch>
2023-12-01 14:40:37 +0100

    exercise 11

---
+++ week_11_solutions/Exercise_1/cpi_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --job-name="cpi_openacc"
+#SBATCH --account="uzg2"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=chiara.zanatta@uzh.ch
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+srun cpi_openacc
