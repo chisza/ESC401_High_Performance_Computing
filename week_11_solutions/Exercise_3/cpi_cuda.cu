6b8614d40beefc2fb23e3712f458abd187f889c9
Chiara Zanatta <chiara.zanatta@hispeed.ch>
2023-12-07 20:23:25 +0100

    exercise 11.3

--- week_11_solutions/Exercise_3/cpi_cuda.cu
+++ week_11_solutions/Exercise_3/cpi_cuda.cu
@@ -4,8 +4,8 @@
 extern "C" double getTime(void);
 
 #define NBIN 1000000000  // Number of bins
-#define NUM_BLOCK  (2*56)  // Number of thread blocks
-#define NUM_THREAD  (2*8)  // Number of threads per block
+//#define NUM_BLOCK  (300)  // Number of thread blocks
+//#define NUM_THREAD  (80)  // Number of threads per block
 
 // Kernel that executes on the CUDA device
 __global__ void cal_pi(double *sum, int nbin, double step, int nthreads, int nblocks) {
@@ -20,6 +20,15 @@ __global__ void cal_pi(double *sum, int nbin, double step, int nthreads, int nbl
 
 // Main routine that executes on the host
 int main(void) {
+    int num_blocks[8] = {60, 120, 180, 240, 300, 360, 420, 600};
+    int num_threads[10] = {16, 32, 48, 64, 80, 96, 112, 128, 144, 160};
+
+    for (int i; i < 8; i++) {
+        for (int j; j < 10; j++) {
+            int NUM_BLOCK = num_blocks[i];
+            int NUM_THREAD = num_threads[j]
+        }
+    }
 	dim3 dimGrid(NUM_BLOCK,1,1);  // Grid dimensions
 	dim3 dimBlock(NUM_THREAD,1,1);  // Block dimensions
 	double *sumHost, *sumDev;  // Pointer to host & device arrays
@@ -44,7 +53,7 @@ int main(void) {
 
 	// Print results
 	double delta = getTime() - start;
-	printf("PI = %.16g computed in %.4g seconds\n", pi, delta);
+	printf("PI = %.16g computed in %.4g seconds, on NUM_BLOCK: %d, NUM_THREAD: %d\n", pi, delta, NUM_BLOCK, NUM_THREAD);
 	// Cleanup
 	free(sumHost);
 	cudaFree(sumDev);
