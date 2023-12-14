f1839548a9728aafece1b21ead69211a3cf2a064
Chiara Zanatta <chiara.zanatta@hispeed.ch>
2023-12-14 09:55:52 +0100

    exercise 12 jobscripts

---
+++ week_12_solutions/axpy/axpy_jobscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --job-name="blur"
+#SBATCH --account="uzg2"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=chiara.zanatta@uzh.ch
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=debug
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+
+for N in {0..1000..100}
+do
+  echo "N = $N"
+  srun axpy.openacc $N
+done
